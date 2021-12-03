f2200a9cc0e97e27fc0da0d87d66e92ba524496a
mnsosa <mnsosa.ms@gmail.com>
2021-12-03 14:08:54 -0300

    Part 2 completed

--- day_3.py
+++ day_3.py
@@ -25,15 +25,18 @@ with open("data_3.txt") as f:
     i = 0
     while len(data_o2) > 1:
         # sum cols and get the i-col max value
-        max = int(np.sum(data_o2, axis=0)[i] >= len(data_o2)/2)
-        
+        max = int(np.sum(data_o2, axis=0)[i] >= len(data_o2)/2.)
+        data_o2 = [elem for elem in data_o2 if elem[i] != max]
+        i += 1
+    
+    i = 0
+    while len(data_co2) > 1:
         # get the min per col
         min = int(not (np.sum(data_co2, axis=0)[i] >= len(data_co2)/2))
-
-        data_o2 = [elem for elem in data_o2 if elem[i] != max]
         data_co2 = [elem for elem in data_co2 if elem[i] != min]
-        
         i += 1
+        
+        
 
     data_o2_str = "".join(map(str, data_o2[0]))
     data_co2_str = "".join(map(str, data_co2[0]))
@@ -42,6 +45,4 @@ with open("data_3.txt") as f:
     data_co2_dec = bin2dec(data_co2_str)
 
     print("Part 2: ", data_o2_dec*data_co2_dec)
-    print("Part 2: ", data_o2_dec, data_co2_dec)
-    print(data_o2_str, data_co2_str)
 
